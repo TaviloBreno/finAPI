8533df7271f5b45389a0aa4fdcdee8fb54461567
Tavilo Breno <breno_wk2@hotmail.com>
2023-03-01 18:10:41 -0300

    Criando um Middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyExistsAccountCPF(req, res, next){
+    const { cpf } = req.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return res.status(400).json({error: 'Customer not found!'});
+    }
+
+    return next();
+}
+
 app.post('/account', (req, res) => {
     const { cpf, name } = req.body;
 
@@ -28,16 +40,10 @@ app.post('/account', (req, res) => {
     return res.status(201).send();
 });
 
-app.get('/statement', (req, res) => {
-   const { cpf } = req.headers;
+app.get('/statement', verifyExistsAccountCPF, (req, res) => {
+    const { customer } = req;
 
-   const customer = customers.find((customer) => customer.cpf === cpf);
-
-   if (!customer) {
-       return res.status(400).json({error: 'Customer not found!'});
-   }
-
-   return res.json(customer.statement);
+      return res.json(customer.statement);
 });
 
 app.listen(3333);
